8e49410f6df8d5633f6c43e365a8463f6c1fe500
no1here0003 <nate.winstel@insight.com>
2022-06-21 18:39:27 -0400

    Disable

--- hack/fluxcd.sh
+++ hack/fluxcd.sh
@@ -2,16 +2,14 @@
 
 kubectl create ns flux-system
 sleep 5
-kubectl apply -f ~/secrets/cluster-secrets2.yaml
-kubectl apply -f ~/secrets/cluster-settings2.yaml
 cat ~/.config/sops/age/keys.txt | kubectl -n flux-system create secret generic sops-age \
 --from-file=age.agekey=/dev/stdin
 
 
 
 flux bootstrap github \
-  --owner=no1here0003 \
-  --repository=kubeadm-cluster \
+  --owner=nwinstel-insight \
+  --repository=kube-cluster \
   --branch=main \
   --path=./cluster/base \
   --personal
