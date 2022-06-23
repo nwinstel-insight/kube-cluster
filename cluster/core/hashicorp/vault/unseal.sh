c251da1bf24334427d1722c92d1144742696ca4b
no1here0003 <nate.winstel@insight.com>
2022-06-23 15:48:26 -0400

    Enable kiali

--- cluster/core/hashicorp/vault/unseal.sh
+++ cluster/core/hashicorp/vault/unseal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#export VAULT_SEAL1=""
-#export VAULT_SEAL2=""
-#export VAULT_SEAL3=""
+export VAULT_SEAL1="KvNdar7+nHs019UKidhL3rq09m1573C7bbaFUx17VKG2"
+export VAULT_SEAL2="ZoRN3FimChB8vifwrZFot0H4SkmuwjDbd4sJ6fB0cvSJ"
+export VAULT_SEAL3="9UnRXilAqlPUa1SGyQrNk1JiUNtuYA0IxObTnCEh6i9F"
 
 kubectl exec --stdin=true --tty=true vault-0 -n hashicorp -- vault operator unseal $VAULT_SEAL1
 kubectl exec --stdin=true --tty=true vault-1 -n hashicorp -- vault operator unseal $VAULT_SEAL1
